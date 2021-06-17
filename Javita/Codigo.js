7d9dfe463ee792b1842d6c54d439a80c8fa400e6
xicodami <80936314+xicodami@users.noreply.github.com>
2021-06-16 21:01:16 -0500

    Inicio Login

---
+++ Javita/Codigo.js
@@ -0,0 +1,9 @@
+// alert("Hola queridos aprendices")
+
+//Document.write("Buena los estudiantes")
+
+// prompt("Escriba su nombre")
+
+//var = valor (numerico, caracteres, Boolean)
+//let = global
+//const = constante :v
