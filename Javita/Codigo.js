a2312e73e9b8ab208abac5f1c7ce73101e00d91b
xicodami <80936314+xicodami@users.noreply.github.com>
2021-06-21 07:04:32 -0500

    Actualizacion Daniel

--- Javita/Codigo.js
+++ Javita/Codigo.js
@@ -7,3 +7,11 @@
 //var = valor (numerico, caracteres, Boolean)
 //let = global
 //const = constante :v
+
+var ractive = new Ractive({
+    el: '#container',
+    template: '#template',
+    data: {}
+  });
+  
+  
