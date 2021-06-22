787644fc9885f51bab454cf6f9eb7794e0067490
xicodami <80936314+xicodami@users.noreply.github.com>
2021-06-21 22:52:46 -0500

    Proximo merge

--- Javita/login.js
+++ Javita/login.js
@@ -1,20 +1,4 @@
-// alert("Hola queridos aprendices")
-
-//Document.write("Buena los estudiantes")
-
-// prompt("Escriba su nombre")
-
-//var = valor (numerico, caracteres, Boolean)
-//let = global
-//const = constante :v
-
-var ractive = new Ractive({
-    el: '#container',
-    template: '#template',
-    data: {}
-  });
-  
-  console.clear();
+console.clear();
 
 const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
